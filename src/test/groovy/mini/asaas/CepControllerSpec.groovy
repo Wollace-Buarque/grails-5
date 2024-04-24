81594a57e5c4d381c0b5e0f434f51e2b9d2a8016
Wollace Buarque <wollacebuarque@outlook.com>
2024-04-24 20:05:18 -0300

    feat: Add completed project

---
+++ src/test/groovy/mini/asaas/CepControllerSpec.groovy
@@ -0,0 +1,18 @@
+package mini.asaas
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class CepControllerSpec extends Specification implements ControllerUnitTest<CepController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
